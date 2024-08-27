2cd9e0232c796c3464dbd0b8f114bf18e31daf65
justrals <justrals.uwu@gmail.com>
2024-08-26 22:13:37 -0300

    Update welcome-message.plugin.zsh

--- welcome-message.plugin.zsh
+++ welcome-message.plugin.zsh
@@ -6,49 +6,88 @@ term_width=$(tput cols)
 # Text centering function
 center_text() {
     local text="$1"
-    local color="$2"
-    local text_length=${#text}
-    local padding=$(( (term_width - text_length) / 2 ))
-    printf "%*s\e[%sm%s\e[0m\n" $padding "" "$color" "$text"
+    local padding=$(( (term_width - ${#text}) / 2 ))
+    printf "%*s%s\n" $padding "" "$text"
+}
+
+# Function to get OS type
+get_os() {
+    case "$(uname -s)" in
+        Darwin)
+            echo "macOS"
+            ;;
+        Linux)
+            echo "Linux"
+            ;;
+        MINGW*|MSYS*|MYSYS*)
+            echo "Windows"
+            ;;
+        *)
+            echo "Unknown"
+            ;;
+    esac
 }
 
 # Function to display system information
-print_system_info() {
-    # Colors
-    os_color="34"
-    kernel_color="32"
-    uptime_color="33"
-    shell_color="35"
-    resolution_color="36"
-    cpu_color="31"      
+get_system_info() {
+    os_type=$(get_os)
 
-    # Getting system information
-    os_info="OS: $(sw_vers -productName) $(sw_vers -productVersion)"
-    kernel_info="Kernel: $(uname -r)"
-    uptime_info="Uptime: $(uptime | awk -F'( |,|:)+' '{print $6" days, "$8" hours, "$9" mins"}')"
-    shell_info="Shell: $($SHELL --version | head -n1)"
-    cpu_info="CPU: $(sysctl -n machdep.cpu.brand_string)"
+    case "$os_type" in
+        "macOS")
+            os_info="OS: $(sw_vers -productName) $(sw_vers -productVersion)"
+            kernel_info="Kernel: $(uname -r)"
+            uptime_info="Uptime: $(uptime -p | sed 's/^up //')"
+            shell_info="Shell: $($SHELL --version | head -n1)"
+            cpu_info="CPU: $(sysctl -n machdep.cpu.brand_string)"
+            resolutions=$(system_profiler SPDisplaysDataType | grep "Resolution:" | awk '{print $2 " x " $4}' | head -n 2)
+            ;;
+        "Linux")
+            os_info="OS: $(lsb_release -d | awk -F'\t' '{print $2}')"
+            kernel_info="Kernel: $(uname -r)"
+            uptime_info="Uptime: $(uptime -p | sed 's/^up //')"
+            shell_info="Shell: $($SHELL --version | head -n1)"
+            cpu_info="CPU: $(lscpu | grep 'Model name' | awk -F': ' '{print $2}' | xargs)"
+            resolutions=$(xrandr | grep ' connected' | awk '{print $3}' | sed 's/[^x]*//;s/+.*//;s/,/ & /' | head -n 2)
+            ;;
+        "Windows")
+            os_info="OS: $(cmd.exe /c ver | head -n1)"
+            kernel_info="Kernel: $(uname -r)"
+            uptime_info="Uptime: $(uptime -p | sed 's/^up //')"
+            shell_info="Shell: $($SHELL --version | head -n1)"
+            cpu_info="CPU: $(wmic cpu get Caption | tail -n +2)"
+            resolutions="Resolution information not available."
+            ;;
+        *)
+            os_info="OS: Unknown"
+            kernel_info="Kernel: Unknown"
+            uptime_info="Uptime: Unknown"
+            shell_info="Shell: Unknown"
+            cpu_info="CPU: Unknown"
+            resolutions="Resolution information not available."
+            ;;
+    esac
 
-    # Getting and processing resolutions
-    resolutions=$(system_profiler SPDisplaysDataType | grep "Resolution:" | awk '{print $2 " x " $4}' | head -n 2)
-
-    # Checking if resolutions were found and formatting output
+    # Format resolution info
     if [ -n "$resolutions" ]; then
-        resolution_info="Main: $(echo "$resolutions" | head -n1)"
-        if [ $(echo "$resolutions" | wc -l) -eq 2 ]; then
-            resolution_info="$resolution_info & Second: $(echo "$resolutions" | tail -n1)"
+        if [ $(echo "$resolutions" | wc -l) -eq 1 ]; then
+            resolution_info="Resolution: $(echo "$resolutions" | head -n1)"
+        elif [ $(echo "$resolutions" | wc -l) -eq 2 ]; then
+            resolution_info="Main: $(echo "$resolutions" | head -n1) & Second: $(echo "$resolutions" | tail -n1)"
+        else
+            resolution_info="Resolution data not available."
         fi
     else
         resolution_info="Resolution data not available."
     fi
 
-    # Centering and displaying system information with color
-    center_text "$os_info" "$os_color"
-    center_text "$kernel_info" "$kernel_color"
-    center_text "$uptime_info" "$uptime_color"
-    center_text "$shell_info" "$shell_color"
-    center_text "$resolution_info" "$resolution_color"
-    center_text "$cpu_info" "$cpu_color"
+    # Combine all info into a single string
+    all_info="$(center_text "$os_info")
+$(center_text "$kernel_info")
+$(center_text "$uptime_info")
+$(center_text "$shell_info")
+$(center_text "$resolution_info")
+$(center_text "$cpu_info")"
+    echo "$all_info"
 }
 
 # Main text for figlet
@@ -56,12 +95,15 @@ figlet_text="justrals"
 figlet_font="larry3d"
 
 # Creating text output using figlet
-figlet_output=$(figlet -f "$figlet_font" "$figlet_text" | lolcat)
+figlet_output=$(figlet -f "$figlet_font" "$figlet_text")
 
-# Centering and displaying figlet text
-echo "$figlet_output" | while IFS= read -r line; do
-    center_text "$line" ""
-done
+# Split figlet output into lines and center each line
+centered_figlet_output=$(echo "$figlet_output" | awk -v term_width=$term_width '{printf "%*s\n", (term_width+length($0))/2, $0}')
 
-# Function call for system info display
-print_system_info
+# Combine figlet output and system info into one string
+combined_output="$centered_figlet_output
+
+$(get_system_info)"
+
+# Display the combined output with a single gradient
+echo "$combined_output" | lolcat
